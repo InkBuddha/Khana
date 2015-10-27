a13f20fa4faa1274574e6c3690c1ce1150071c3e
InkBuddha <stachowski.alexandra@hotmail.fr>
2015-10-27 17:35:16 +0100

    create recipe controller + index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 	 'signup'  => 'users#new'
   get 	 'login'	 =>	'sessions#new'
   post   'login'	 =>	'sessions#create'
-  delete 'logout'	 =>	'sessions#destroy'  
+  delete 'logout'	 =>	'sessions#destroy'
+  get 	 '/recipes' => 'recipes#index'
+  get 	 '/recipes/:id' => 'recipes#show', as: :recipe
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
