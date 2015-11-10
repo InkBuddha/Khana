04711573800bcf5f8cf858e59af83920118151a9
InkBuddha <stachowski.alexandra@hotmail.fr>
2015-11-10 16:57:26 +0100

    try to fix errors

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,40 +1,40 @@
 class IngredientsController < ApplicationController
-	before_action :get_ingredient, only: [:show, :edit, :update, :destroy]
+	# before_action :get_ingredient, only: [:show, :edit, :update, :destroy]
 
-	def index
-		@ingredients = Ingredients.all
-	end
+	# def index
+	# 	@ingredients = Ingredients.all
+	# end
 
-	def show
-	end
+	# def show
+	# end
 
-	def new
-		@ingredient = Ingredient.new
-	end
+	# def new
+	# 	@ingredient = Ingredient.new
+	# end
 
-	def edit
-	end
+	# def edit
+	# end
 
-	def create
-		this_recipe = Recipe.find(params[:recipe_id])
-		@item = this_recipe.ingredients.build(ingredient_params)
-		@ingredient = @item.build_ingredient(params[:ingredient])
-	end
+	# def create
+	# 	this_recipe = Recipe.find(params[:recipe_id])
+	# 	@item = this_recipe.ingredients.build(ingredient_params)
+	# 	@ingredient = @item.build_ingredient(params[:ingredient])
+	# end
 
-	def update
-	end
+	# def update
+	# end
 
-	def destroy
-	end
+	# def destroy
+	# end
 
-	private
+	# private
 
-		def ingredient_params
-			params.require(:ingredient).permit(:name)
-		end
+	# 	def ingredient_params
+	# 		params.require(:ingredient).permit(:name)
+	# 	end
 
-		def get_ingredient
-			@ingredient.find(params[:id])
-		end
+	# 	def get_ingredient
+	# 		@ingredient.find(params[:id])
+	# 	end
 
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,6 +8,14 @@ class RecipesController < ApplicationController
 	def show
 	end
 
+	def new_recipe_item
+		@recipe = Item.new
+	end
+
+	def new_recipe_ingredient
+		@recipe = Ingredient.new
+	end
+
 	def new
 		@recipe = Recipe.new
 	end
@@ -32,9 +40,8 @@ class RecipesController < ApplicationController
 
 		def recipe_params
 			params.require(:recipe).permit(
-										:title, :description,
-										items_attributes: [:id, :amount, :measure, :_destroy, 
-										{ingredients_attributes: [:id, :name] }] )
+						:title, :description,
+						items_attributes: [:id, :amount, :measure, :_destroy, ingredients_attributes: [:id, :_destroy, :name]] )
 		end
 
 		# def recipe_params
@@ -43,6 +50,8 @@ class RecipesController < ApplicationController
 		# 								items_attributes: [:id, :amount, :measure, :_destroy, 
 		# 								{ingredients_attributes: :name }] )
 		# end
+
+		#Before filters
 
 		def get_recipe
 			@recipe = Recipe.find(params[:id])
