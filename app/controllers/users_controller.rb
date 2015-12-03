137b2d05ecf2f06d3770ea853280f02a76354440
InkBuddha <stachowski.alexandra@hotmail.fr>
2015-12-03 17:48:37 +0100

    create subnavs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
-      redirect_to edit_user_url
+      redirect_to @user
     else
       render 'edit'
     end
