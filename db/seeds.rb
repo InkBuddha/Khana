5e062a2f77213c047273731c49bcdf8d5b5302e9
InkBuddha <stachowski.alexandra@hotmail.fr>
2015-12-13 19:05:52 +0100

    add users avatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 User.create!(name: "Example User",
 						 email: "example@example.com",
 						 password: "foobar",
-						 password_confirmation: "foobar", 
+						 password_confirmation: "foobar",
 						 admin: true,
 						 activated: true,
 						 activated_at: Time.zone.now)
